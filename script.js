03277103108185bc820edb545736f3ad0758be4c
Taras Radevych <taras.radevych@gmail.com>
2023-08-13 00:17:25 +0300

    scroll

---
+++ script.js
@@ -0,0 +1,35 @@
+function toggleDetails(detailsElement) {
+  const allDetails = document.querySelectorAll(".categories");
+
+  allDetails.forEach(details => {
+      if (details !== detailsElement) {
+          details.open = false;
+      }
+  });
+
+  detailsElement.open = detailsElement.open;
+
+  if (detailsElement.open) {
+      const summaryElement = detailsElement.querySelector(".categories-title");
+      if (summaryElement) {
+          summaryElement.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+  }
+}
+function toggleAndScroll(summaryElement) {
+  const detailsElement = summaryElement.parentElement;
+  const allDetails = document.querySelectorAll(".categories");
+
+  allDetails.forEach(details => {
+      if (details !== detailsElement) {
+          details.open = true;
+      }
+  });
+
+  detailsElement.open = !detailsElement.open;
+
+  if (detailsElement.open) {
+      // Прокрутка до верху сторінки
+      window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+}
